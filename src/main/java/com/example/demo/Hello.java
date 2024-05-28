331065790af3f37d44e844bec4d255a4defb566d
fanpeilun <fanpeilun@163.com>
2024-05-28 17:24:49 +0800

    first commit

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class Hello {
+    
+    @GetMapping("/")
+    public String hi() {
+        return "Hello Demo!";
+    }
+
+}
